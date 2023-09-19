083c2a4648c691f662bd2cb91b4707364d7d862e
nicolevbiene <nicolevanbiene@gmail.com>
2023-09-19 13:02:21 +0200

    menu

---
+++ compulsory2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    bool isRunning = true;
+    
+    while (isRunning)
+    {
+        int choice;
+        cout << "1) Option 1" << endl;
+        cout << "2) Option 2" << endl;
+        cout << "3) Option 3" << endl;
+        cout << "4) Quit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch (choice)
+        {
+            case 1:
+                cout << "you chose option 1\n";
+                break;
+            case 2:
+                cout << "you chose option 2\n";
+                break;
+            case 3:
+                cout << "you chose option 3\n";
+                break;
+            case 4:
+                cout << "you chose to quit\n";
+                isRunning = false;
+                break;
+            default:
+                break;
+        }
+    }
+    cout << "Good Bye";
+}
