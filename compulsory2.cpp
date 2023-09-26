911e82ec19d5027a5fd675762ceb75664ae5408e
nicolevbiene <nicolevanbiene@gmail.com>
2023-09-26 13:50:16 +0200

    added simple math and factorial

--- compulsory2.cpp
+++ compulsory2.cpp
@@ -1,7 +1,98 @@
 #include <iostream>
 using namespace std;
 
+void SimpleMath()
+{
+    bool isRunning = true;
 
+    while (isRunning)
+    {
+        int numberA;
+        cout << "please enter a number and press enter\n";
+        cin >> numberA;
+        char operators;
+        cout << "please enter a operator and press enter (+ - * /)\n";
+        cin >> operators;
+        int numberB;
+        cout << "please enter a number and press enter\n";
+        cin >> numberB;
+        double result;
+
+        switch (operators)
+        {
+            case '+':
+                result = numberA + numberB;
+                break;
+            case '-':
+                result = numberA - numberB;
+                break;
+            case '*':
+            result = numberA * numberB;
+                break;
+            case '/':
+                result = double(numberA) / double(numberB);
+                break;
+        }
+        cout << "Result:" << result << endl;
+
+        cout << "Do you want to continue?\n";
+
+        bool isContinue = true;
+        
+        while (isContinue)
+        {
+            char desision;
+            cin >> desision;
+
+            switch (desision)
+            {
+                case 'Y':
+                    isContinue = false;
+                    break;
+                case 'N':
+                    isRunning = false; 
+                    isContinue = false;
+                    break;
+                default:
+                    cout << "invalid choice, choose Y or N:\n";
+                    cin.clear();
+                    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                    break;
+            }
+        }
+    }
+}
+
+int Factorial(int number)
+{
+    if (number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return (number * Factorial(number-1));
+    }   
+}
+
+void setUpFactorial()
+{
+    int number;
+    int result;
+
+    cout << "enter a number\n";
+    cin >> number;
+    if (number < 0)
+    {
+        cout << "negative number not alout\n";
+    }
+    else
+    {
+        cout << "Result:";
+        result = Factorial(number);
+        cout << result << endl;
+    }
+}
 
 int main()
 {
@@ -10,9 +101,9 @@ int main()
     while (isRunning)
     {
         int choice;
-        cout << "Options:" << endl;
-        cout << "1) Option 1" << endl;
-        cout << "2) Option 2" << endl;
+        //cout << "Options:" << endl;
+        cout << "1) SimpleMath" << endl;
+        cout << "2) setUpFactorial" << endl;
         cout << "3) Option 3" << endl;
         cout << "4) Quit" << endl;
         cout << "Enter choice: ";
@@ -21,10 +112,12 @@ int main()
         switch (choice)
         {
             case 1:
-                cout << "you chose option 1\n";
+                cout << "you chose SimpleMath\n";
+                SimpleMath();
                 break;
             case 2:
-                cout << "you chose option 2\n";
+                cout << "you chose setUpFactorial\n";
+                setUpFactorial();
                 break;
             case 3:
                 cout << "you chose option 3\n";
@@ -35,6 +128,8 @@ int main()
                 break;
             default:
                 cout << "invalid choice, choose again:\n";
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
                 break;
         }
     }
